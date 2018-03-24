d242a41934fba878e7939105dc75ddf7bb3237df
Choi Rak_Kyoung <knreipiel@hotmail.com>
2018-03-24 22:19:01 +0900

    add XOR gate

--- gate.py
+++ gate.py
@@ -20,3 +20,11 @@ def OR(x1, x2):
     w = np.array([0.5, 0.5])
     b = -0.2
     return np.sum(w * x) + b > 0
+
+
+# 2 stage perceptron
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
